950ac67fe0f2f9bfb9f5d3d1a443f9beb8e26ebb
Sciss <contact@sciss.de>
2013-05-16 22:41:26 +0200

    comparison works

--- src/main/scala/de/sciss/mellite/gui/impl/RecursionFrameImpl.scala
+++ src/main/scala/de/sciss/mellite/gui/impl/RecursionFrameImpl.scala
@@ -14,7 +14,9 @@ import language.reflectiveCalls
 import de.sciss.synth.proc
 import de.sciss.processor.Processor
 import scala.util.{Success, Failure}
-import javax.swing.JPanel
+import de.sciss.processor.impl.ProcessorImpl
+import de.sciss.synth.io.AudioFile
+import concurrent.blocking
 
 object RecursionFrameImpl {
   def apply[S <: Sys[S]](doc: Document[S], elem: Element.Recursion[S])
@@ -29,6 +31,47 @@ object RecursionFrameImpl {
       view.guiInit()
     }
     view
+  }
+
+  private final class FileComparison(a: File, b: File) extends ProcessorImpl[Boolean, FileComparison] {
+    protected def body(): Boolean = blocking {
+      val inA = AudioFile.openRead(a)
+      try {
+        val inB = AudioFile.openRead(b)
+        try {
+          val numCh     = inA.numChannels
+          val numFrames = inA.numFrames
+          if (numCh           != inB.numChannels ||
+              numFrames       != inB.numFrames   ||
+              inA.sampleRate  != inB.sampleRate) return false
+
+          val bufSz   = 8192
+          val bufA    = inA.buffer(bufSz)
+          val bufB    = inB.buffer(bufSz)
+          var remain  = numFrames
+          while (remain > 0) {
+            val chunk = math.min(bufSz, remain).toInt
+            inA.read(bufA, 0, chunk)
+            inB.read(bufB, 0, chunk)
+            for (ch <- 0 until numCh) {
+              val ca = bufA(ch)
+              val cb = bufB(ch)
+              for (i <- 0 until chunk) {
+                if (ca(i) != cb(i)) return false
+              }
+            }
+            remain -= chunk
+            checkAborted()
+          }
+          true
+
+        } finally {
+          inB.close()
+        }
+      } finally {
+        inA.close()
+      }
+    }
   }
 
   private final class Impl[S <: Sys[S]](val document: Document[S], elem: stm.Source[S#Tx, Recursion[S]],
@@ -72,27 +115,29 @@ object RecursionFrameImpl {
       }
 
       def performMatch() {
-        val (groupH, gain, span, channels, spec) = _cursor.step { implicit tx =>
+        val (groupH, gain, span, channels, audio) = _cursor.step { implicit tx =>
           import proc.ProcGroup.serializer
           val e         = elem()
           val _groupH   = tx.newHandle(e.group)
           val _gain     = e.gain
           val _span     = e.span
-          val _spec     = e.deployed.entity.value.spec
+          val _audio    = e.deployed.entity.value
           val _channels = e.channels
-          (_groupH, _gain, _span, _channels, _spec)
+          (_groupH, _gain, _span, _channels, _audio)
         }
         val file              = File.createTempFile("bounce", ".w64")
         val server            = Server.Config()
-        ActionBounceTimeline.specToServerConfig(file, spec, server)
+        ActionBounceTimeline.specToServerConfig(file, audio.spec, server)
 
         val pSet    = ActionBounceTimeline.PerformSettings(groupH, server, gain, span, channels)
-        val p       = ActionBounceTimeline.perform(document, pSet)
-        monitor("Match", p) { _ =>
-          GUI.defer {
-            println("Match bounce done.")
+        val pBounce = ActionBounceTimeline.perform(document, pSet)
+        monitor("Match", pBounce) { _ =>
+          val pCompare = new FileComparison(audio.artifact, file)
+          pCompare.start()
+          monitor("Compare", pCompare) { res =>
             processStopped()
-            file.delete()
+            println(if (res) "Is up to date" else "Has changed")
+            // println(s"Old file: ${audio.artifact}, new file $file")
           }
         }
       }
@@ -101,13 +146,16 @@ object RecursionFrameImpl {
         matchDeployed.enabled = true
         ggProgress.visible    = false
         ggStopProcess.visible = false
+        funStopProcess        = () => ()
       }
 
+      // `onSuccess` is called on EDT!
       def monitor[A](title: String, p: Processor[A, _])(onSuccess: A => Unit) {
         matchDeployed.enabled = false
         ggProgress.visible    = true
         ggStopProcess.visible = true
         ggStopProcess.requestFocus()
+        funStopProcess        = () => p.abort()
 
         p.addListener {
           case prog @ Processor.Progress(_, _) => GUI.defer {
@@ -115,7 +163,7 @@ object RecursionFrameImpl {
           }
         }
         p.onComplete {
-          case Success(value) => onSuccess(value)
+          case Success(value) => GUI.defer(onSuccess(value))
           case Failure(Processor.Aborted()) =>
             GUI.defer(processStopped())
           case Failure(e: Exception) => // XXX TODO: Desktop should allow Throwable for DialogSource.Exception
