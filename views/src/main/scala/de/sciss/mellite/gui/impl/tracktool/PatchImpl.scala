8cfa664b02ab249c970569c5ba49c4257946063b
Hanns Holger Rutz <contact@sciss.de>
2014-07-14 09:09:30 +0200

    patch tool ok

--- views/src/main/scala/de/sciss/mellite/gui/impl/tracktool/PatchImpl.scala
+++ views/src/main/scala/de/sciss/mellite/gui/impl/tracktool/PatchImpl.scala
@@ -17,9 +17,8 @@ package impl
 package tracktool
 
 import java.awt.{Color, RenderingHints, Point, Toolkit}
+import de.sciss.desktop.Desktop
 import de.sciss.synth.proc.{Obj, Proc}
-import de.sciss.mellite.gui.TimelineProcCanvas
-import de.sciss.mellite.gui.TrackTool
 import de.sciss.lucre.expr.Expr
 import de.sciss.span.SpanLike
 import java.awt.image.BufferedImage
@@ -32,14 +31,14 @@ import de.sciss.mellite.gui.impl.component.PaddedIcon
 import scala.swing.Insets
 
 object PatchImpl {
-  private lazy val image: BufferedImage = {
+  private def mkImage(aa: Boolean): BufferedImage = {
     val img   = new BufferedImage(17, 17, BufferedImage.TYPE_INT_ARGB)
     val g     = img.createGraphics()
     val shp1  =   new Area(new Ellipse2D.Float(0, 0, 17, 17))
     shp1 subtract new Area(new Ellipse2D.Float(5, 5,  7,  7))
     val shp2  =   new Area(new Ellipse2D.Float(1, 1, 15, 15))
     shp2 subtract new Area(new Ellipse2D.Float(4, 4,  9,  9))
-    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    if (aa) g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
     g.setColor(Color.white)
     g.fill(shp1)
     g.setColor(Color.black)
@@ -48,8 +47,11 @@ object PatchImpl {
     img
   }
 
+  lazy val image: BufferedImage = mkImage(aa = true)
+
+  // anti-aliased transparency seem unsupported on Linux
   private lazy val cursor =
-    Toolkit.getDefaultToolkit.createCustomCursor(image, new Point(8, 8), "patch")
+    Toolkit.getDefaultToolkit.createCustomCursor(mkImage(aa = Desktop.isMac), new Point(8, 8), "patch")
 }
 final class PatchImpl[S <: Sys[S]](protected val canvas: TimelineProcCanvas[S])
   extends RegionImpl[S, TrackTool.Patch[S]] with Dragging[S, TrackTool.Patch[S]] {
@@ -60,21 +62,24 @@ final class PatchImpl[S <: Sys[S]](protected val canvas: TimelineProcCanvas[S])
   val name          = "Patch"
   val icon          = new PaddedIcon(new ImageIcon(PatchImpl.image), new Insets(1, 1, 2, 2)) // make it 20x20
 
-  protected type Initial = TimelineObjView[S]
+  protected type Initial = ProcView[S] // TimelineObjView[S]
 
   protected def dragToParam(d: Drag): Patch[S] = {
     val pos   = d.currentPos
     val sink  = canvas.findRegion(frame = pos, hitTrack = d.currentTrack) match {
-      case Some(r: ProcView[S]) if r != d.initial /* && r.inputs.nonEmpty */ =>  // region.inpus only carries linked ones!
+      case Some(r: ProcView[S]) if r != d.initial /* && r.inputs.nonEmpty */ =>  // region.inputs only carries linked ones!
         Patch.Linked(r)
       case _ =>
         Patch.Unlinked(frame = pos, y = d.currentEvent.getY)
     }
-    ??? // Patch(d.initial, sink)
+    Patch(d.initial, sink)
   }
 
   protected def handleSelect(e: MouseEvent, hitTrack: Int, pos: Long, region: TimelineObjView[S]): Unit =
-    /* if (region.outputs.nonEmpty) */ new Drag(e, hitTrack, pos, region)  // region.outputs only carries linked ones!
+    region match {
+      case pv: ProcView[S] => new Drag(e, hitTrack, pos, pv) // region.outputs only carries linked ones!
+      case _ =>
+    }
 
   protected def commitObj(drag: Patch[S])(span: Expr[S, SpanLike], out: Obj[S])(implicit tx: S#Tx): Unit =
     (drag.sink, out) match {
